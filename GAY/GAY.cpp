a99fccb31b8aa5a141e2cab82e9d9e16b5a69f80
gameschool-student1 <stud1.gschl24@outlook.com>
2025-06-06 19:02:31 +0700

    MY Novell

--- GAY/GAY.cpp
+++ GAY/GAY.cpp
@@ -85,7 +85,7 @@ void distance() {
 
 
 
-void WhiteMores() {
+void Street() {
 
     std::string question = "Понятно, дело твоё, но ты знаещь наши правила, говори пароль\n";
     std::string pasword1 = "Кнут";
@@ -108,7 +108,37 @@ void WhiteMores() {
     {
         std::cout << "Тебе мозги вправить, а то мне тут уже стоять счучно. НЕ собираюсь я от тебя это выслушивать\n";
     }
+    
+       
+    
+    
+
+
+
 }
+
+void Bar() {
+
+    std::string sentence = "Выйграл у очередных олухов пару десятков зеленых\n";
+    std::string reaction1 = "Ты будто в этой бухте живешь. Как не приду, ты всегда тут";
+    std::string reaction2 = "Твоей вежливости нет предела, как и твоему сарказму";
+    std::string reaction3 = "Скорее наколол очередных лохов, обычно ты этим занимаешся";
+    std::string otvet;
+      
+
+    std::cout << "Доброго вечера, мой заблудший друг\n";
+    std::cout << "Как видешь пока тебя не было, я зря вмемя не терял";
+
+    if (otvet == reaction1)
+    {
+        std::cout << "Ты"
+    }
+
+
+
+}
+
+
 
 
 
@@ -132,7 +162,7 @@ int main()
     setlocale(LC_ALL, "Russian");
     //game();
     //start();
-    WhiteMores();
+    Street();
 }
 
 
