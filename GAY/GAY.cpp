201a76ef7308579e2df832eac1ab6b271b248cdf
Student-3 GameSchool <stud3.gsch124@outlook.com>
2025-06-12 18:00:37 +0700

    My NOVELLA

--- GAY/GAY.cpp
+++ GAY/GAY.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <Windows.h>
 void distance();
-
+void Bar();
 
 
 
@@ -100,6 +100,7 @@ void Street() {
     if (otvet == pasword1)
     {
         std::cout << "Молодец, знаешь наше исинное призвание, заходи\n";
+        Bar();
     }
     else if (otvet == pasword1) {
         std::cout << "Что, решил потопить за собирателей урожая, ты либо пьян, либо отупел\n";
@@ -110,60 +111,88 @@ void Street() {
     }
     
        
-    
-    
+}
 
-
+void Fight() {
 
 }
+
+
+
+
 
 void Bar() {
 
     std::string sentence = "Выйграл у очередных олухов пару десятков зеленых\n";
-    std::string reaction1 = "Ты будто в этой бухте живешь. Как не приду, ты всегда тут";
-    std::string reaction2 = "Твоей вежливости нет предела, как и твоему сарказму";
-    std::string reaction3 = "Скорее наколол очередных лохов, обычно ты этим занимаешся";
-    std::string otvet;
-      
+    std::string reaction1 = "Ты будто в этой бухте живешь. Как не приду, ты всегда тут\n";
+    std::string reaction2 = "Твоей вежливости нет предела, как и твоему сарказму\n";
+    std::string reaction3 = "Скорее наколол очередных лохов, обычно ты этим занимаешся\n";
+
+    int otvet;
+
 
     std::cout << "Доброго вечера, мой заблудший друг\n";
-    std::cout << "Как видешь пока тебя не было, я зря вмемя не терял";
+    std::cout << "Как видешь пока тебя не было, я зря вмемя не терял\n";
+    std::cout << sentence << std::endl << "1. " << reaction1 << "2. " << reaction2 << "3. " << reaction3;
+    std::cin >> otvet;
 
-    if (otvet == reaction1)
+    switch (otvet)
     {
-        std::cout << "Ты"
+        case 1: 
+            std::cout << "Ты знаещь в округе похожую дыру где можно проворачивать такие схемы" << "\n";
+            break;
+        
+
+        case 2: 
+
+            std::cout << "Я не позволяю себе вести подобно этому пьющему сброду, невеже Джимми и дубине Монти. Я с уважением отношусь даже к своему врагу " << "\n";
+            break;
+        
+        case 3: 
+
+
+            std::cout << "Они сами виноваты, что целенаправлено несут мне свои деньги, не зная моих правил" << "\n";
+            break;
+        
+
+          deafault: 
+              std::cout << "ZALUPA" << "\n";
+              break;
+        
+
+
+
     }
+
+    std::cout << "Кстати, а где Джимми и Монти?\n";
+    std::cout << "'Бритва' около бара, ножы свои в стоб метает\n";
+    std::cout << "Монти где-то ходит, как всегда\n";
+
+
+
+
+
+
 
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 //
 int main()
-   {
-    SetConsoleCP(1251);
-    srand(time(NULL));
-    setlocale(LC_ALL, "Russian");
-    //game();
-    //start();
-    Street();
+{
+
+ SetConsoleCP(1251);
+ srand(time(NULL));
+ setlocale(LC_ALL, "Russian");
+ //game();
+ //start();
+ //Street();
+ Bar();
+ 
 }
+
 
 
 
